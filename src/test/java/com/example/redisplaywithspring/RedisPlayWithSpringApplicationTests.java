b73210160c886caac87f85b85726308dcc1cdfbf
t-yoguro <t-yoguro@yahoo-corp.jp>
2019-11-08 18:00:01 +0900

    init project

---
+++ src/test/java/com/example/redisplaywithspring/RedisPlayWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.redisplaywithspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisPlayWithSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
